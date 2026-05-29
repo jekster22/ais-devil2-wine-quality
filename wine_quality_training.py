7f844846138a2c27e125da7a8692a1e82bed7cc0
Javid <javid22h@gmail.com>
2026-05-29 10:27:56 +0200

    200 estimators 15 maxDepth

--- wine_quality_training.py
+++ wine_quality_training.py
@@ -22,7 +22,7 @@ def main():
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     
     with mlflow.start_run():
-        model = RandomForestClassifier(n_estimators=50, max_depth=5, random_state=42)
+        model = RandomForestClassifier(n_estimators=200, max_depth=15, random_state=42)
         model.fit(X_train, y_train)
         
         preds = model.predict(X_test)
